3b82c70af8b83f5b2d17c2277cd801df8706f77c
Ben Mather <brmather1@gmail.com>
2017-10-24 23:17:21 +0100

    Difficult to work out how to send arrays that get allocated later in the code
    1. Put it in a module (not thread-safe, apparently)
    2. Send it to a python callback function (difficult!)

--- quagmire/fortran/mesh.f90
+++ quagmire/fortran/mesh.f90
@@ -45,16 +45,87 @@ subroutine remove_dups ( n, array_in, array_out, k )
   return
 end subroutine remove_dups
 
-recursive subroutine quicksort (a, b, first, last)
+! module qsort_mod
+! implicit none
+ 
+! type group
+!   integer :: order    ! original order of unsorted data
+!   real :: value       ! values to be sorted by
+! end type group
+ 
+! contains
+ 
+! recursive subroutine QSort(a,na)
+ 
+! ! DUMMY ARGUMENTS
+! integer, intent(in) :: nA
+! type (group), dimension(nA), intent(in out) :: A
+ 
+! ! LOCAL VARIABLES
+! integer :: left, right
+! real :: random
+! real :: pivot
+! type (group) :: temp
+! integer :: marker
+ 
+!     if (nA > 1) then
+ 
+!         call random_number(random)
+!         pivot = A(int(random*real(nA-1))+1)%value   ! random pivor (not best performance, but avoids worst-case)
+!         left = 0
+!         right = nA + 1
+ 
+!         do while (left < right)
+!             right = right - 1
+!             do while (A(right)%value > pivot)
+!                 right = right - 1
+!             end do
+!             left = left + 1
+!             do while (A(left)%value < pivot)
+!                 left = left + 1
+!             end do
+!             if (left < right) then
+!                 temp = A(left)
+!                 A(left) = A(right)
+!                 A(right) = temp
+!             end if
+!         end do
+ 
+!         if (left == right) then
+!             marker = left + 1
+!         else
+!             marker = left
+!         end if
+ 
+!         call QSort(A(:marker-1),marker-1)
+!         call QSort(A(marker:),nA-marker+1)
+ 
+!     end if
+ 
+! end subroutine QSort
+ 
+! end module qsort_mod
+
+recursive subroutine sort_quick (n, a, idx, first, last)
 !*****************************************************************************
-! quicksort.f -*-f90-*-
+! Sorting using the quick sort algorithm.
+! a is sorted in-place
+! idx is the integers that would sort the array
+! first and last specify where in the array is to be sorted
+! 
+! idx should be a range of integers from 1 to n
+! 
+! sort_quick.f -*-f90-*-
 ! Author: t-nissie
 ! License: GPLv3
-! Gist: https://gist.github.com/t-nissie/479f0f16966925fa29ea
+! Gist: https://gist.githuidx.com/t-nissie/479f0f16966925fa29ea
   implicit none
-  integer ( kind = 4 ) a(*), b(*), x, t
+  integer ( kind = 4 ) n
+  integer ( kind = 4 ) a(n)
+  integer ( kind = 4 ) idx(n)
+  real ( kind = 8 ) x, t
   integer ( kind = 4 ) first, last
-  integer ( kind = 4 ) i, j
+  integer ( kind = 4 ) i, j, ti
 
   x = a( (first+last) / 2 )
   i = first
@@ -67,69 +138,85 @@ recursive subroutine quicksort (a, b, first, last)
       j=j-1
     end do
     if (i >= j) exit
-    t = a(i);  a(i) = a(j);  a(j) = t
-    b(i) = i; b(j) = j
+    t = a(i);  a(i) = a(j);  a(j) = int(t, 4)
+    ti= idx(i);  idx(i) = idx(j);  idx(j) = ti
     i=i+1
     j=j-1
   end do
-  if (first < i-1) call quicksort(a, b, first, i-1)
-  if (j+1 < last)  call quicksort(a, b, j+1, last)
-end subroutine quicksort
+  if (first < i-1) call sort_quick(n, a, idx, first, i-1)
+  if (j+1 < last)  call sort_quick(n, a, idx, j+1, last)
+end subroutine sort_quick
 
-function cumsum (n, array)
+subroutine cumsum ( n, array, array_sum )
 !*****************************************************************************
 ! cumsum evaluates the cumulative sum on an array
   implicit none
   integer ( kind = 4 ) n
   integer ( kind = 4 ) array(n)
-  integer ( kind = 4 ) cumsum(n)
+  integer ( kind = 4 ) array_sum(n)
   integer ( kind = 4 ) i
 
-  cumsum(:) = array(:)
+  array_sum(:) = 0
+  array_sum(1) = array(1)
   
   do i = 2, n
-    cumsum(i) = cumsum(i) + cumsum(i-1)
+    array_sum(i) = array_sum(i-1) + array(i)
   end do
   return
-end function cumsum
+end subroutine cumsum
+
+! function cumsum ( n, arr )
+
+!   implicit none
+!   integer ( kind = 4 ) n
+!   integer ( kind = 4 ) arr(n)
+!   integer ( kind = 4 ) j, i
+!   integer ( kind = 4 ), intent(out) :: cumsum(n)
+
+!   do j = 2, n
+!     cumsum(j) = cumsum(j-1) + arr(j)
+!   end do
+!   return
+! end function cumsum
 
 subroutine edgesort (n, edges)
 !*****************************************************************************
-! sorts edges by rows with lowest integer first
+! Sorts edges by rows with lowest integer first
+! 
+! if the lowest integer is in the second row, then the order is switched
+! array is returned in-place
   implicit none
   integer ( kind = 4 ) n
-  integer ( kind = 4 ), target :: edges(2,n)
-  integer ( kind = 4 ) ind(n)
+  integer ( kind = 4 ) edges(2,n)
   integer ( kind = 4 ) i
+  integer ( kind = 4 ) a(n)
   logical ( kind = 4 ) mask(n)
-  integer, pointer :: edge
 
-  ind = minloc(edges, dim=1)
-  mask = ind.eq.2
+  do i = 1, n
+    a(i) = i
+  end do
 
-  ! unpack(edges, mask, )
+  mask = minloc(edges, dim=1).eq.2
 
-  ! edge ee
-
-  ! edges(mask) = edges(:mask:-1)
+  edges(:,pack(a, mask)) = edges(2:1:-1,pack(a, mask))
   return
 end subroutine edgesort
 
-function bincount (n, array)
+subroutine bincount ( n, array, nnz )
 !*****************************************************************************
 ! bincount evaluates the number of times an integer appears in an array
   implicit none
   integer ( kind = 4 ) n
   integer ( kind = 4 ) array(n)
-  integer ( kind = 4 ) bincount(maxval(array))
+  integer ( kind = 4 ) nnz(maxval(array))
   integer ( kind = 4 ) i
 
-  bincount(:) = 0
+  nnz = 0
   do i = 1, n
-    bincount(array(i)) = bincount(array(i)) + 1
+    nnz(array(i)) = nnz(array(i)) + 1
   end do
   return
-end function bincount
+end subroutine bincount
 
 subroutine ntriw ( n, x, y, nt, ltri, area, weight )
 !*****************************************************************************
@@ -161,15 +248,15 @@ subroutine ntriw ( n, x, y, nt, ltri, area, weight )
   integer ( kind = 4 ) n,nt,i
   real ( kind = 8 ) x(n),y(n),area(n)
   integer ( kind = 4 ) weight(n)
-  integer ( kind = 4 ) ltri(3,nt)
+  integer ( kind = 4 ), target :: ltri(3,nt)
   real ( kind = 8 ) v1x,v1y,v2x,v2y
-  integer ( kind = 4 ) tri(3)
+  integer ( kind = 4 ), pointer :: tri(:)
 
 !
 ! Get 2 sides of triangle
 !
   do i = 1, nt
-    tri = ltri(:,i)
+    tri => ltri(:,i)
     v1x = x(tri(2)) - x(tri(1))
     v1y = y(tri(2)) - y(tri(1))
     v2x = x(tri(1)) - x(tri(3))
@@ -184,6 +271,169 @@ subroutine ntriw ( n, x, y, nt, ltri, area, weight )
   area = area/6
   return
 end subroutine ntriw
+
+subroutine remove_dups2D (nr, nc, arraya, arrayb)
+!*****************************************************************************
+! remove duplicate entries in a 2D array
+! nc is modified in place to give number of unique columns
+! https://stackoverflow.com/questions/14137610/...
+! remove-repeated-elements-in-a-2d-array-in-fortran
+  implicit none
+
+  integer ( kind = 4 ), intent(in) :: nr  ! number of rows
+  integer ( kind = 4 ), intent(inout) :: nc  ! number of columns
+  integer ( kind = 4 ), intent(in) :: arraya(nr,nc)
+  integer ( kind = 4 ), intent(out) :: arrayb(nr,nc)
+  integer ( kind = 4 ) index_vector(nc)
+  logical ( kind = 4 ) mask(nc)
+  integer ( kind = 4 ) ix, k
+
+  ! First, find the duplicate elements
+  mask(:) = .true.
+  arrayb(:,:) = 0
+
+  do ix = nc, 2, -1
+    mask(ix) = .not.(any(arraya(1,:ix-1)==arraya(1,ix).and.&
+                         arraya(2,:ix-1)==arraya(2,ix)))
+  end do
+
+  k = count(mask)
+
+  index_vector = pack([(ix, ix=1,nc)], mask)
+  arrayb(:,1:k) = arraya(:,index_vector(1:k))
+
+  nc = k
+  return
+end subroutine remove_dups2D
+
+! module something
+!   integer ( kind = 4 ), allocatable :: cloud(:)
+! contains
+subroutine tricloud2 ( nt, ltri, n, callable )
+!*****************************************************************************
+! Finds all neighbours and extended neighbours for every point
+  implicit none
+
+  integer ( kind = 4 ) nt, n
+  integer ( kind = 4 ) ltri(3,nt)
+  integer ( kind = 4 ) edges(2,3*nt)
+  integer ( kind = 4 ) edgey(2,3*nt)
+  integer ( kind = 4 ), allocatable :: row(:), col(:), sort(:)
+  integer ( kind = 4 ), allocatable :: bnnz(:), bnnzsum(:), colsum(:)
+  integer ( kind = 4 ) nr, nc, i, k
+  integer ( kind = 4 ) nnz(n), nnzsum(n)
+  ! integer ( kind = 4 ), intent(out), allocatable :: cloud(n,k)
+  logical ( kind = 4 ), allocatable :: nmask(:)
+  external callable
+  integer ( kind = 4 ), allocatable :: cloud(:)
+  integer ( kind = 4 ) nk
+  
+  ! nnz(maxval(ltri))
+
+  nr = 2; nc = 3*nt
+  write(*,*) "removed dups", maxval(ltri)
+
+  ! slice edges from triangles
+  edges(1,1:nt) = ltri(1,:)
+  edges(2,1:nt) = ltri(2,:)
+  edges(1,nt+1:2*nt) = ltri(1,:)
+  edges(2,nt+1:2*nt) = ltri(3,:)
+  edges(1,2*nt+1:3*nt) = ltri(2,:)
+  edges(2,2*nt+1:3*nt) = ltri(3,:)
+
+  write(*,*) "shapes", shape(edges(1,1:nt)), shape(edges(1,nt:2*nt))
+  write(*,*) "removed dups", maxval(edges), maxval(edgey)
+
+  ! sort edges
+  call edgesort(nc, edges)
+
+  ! remove duplicate entries
+  call remove_dups2D(nr, nc, edges, edgey)
+
+  write(*,*) "removed dups", maxval(edges), maxval(edgey)
+
+  ! allocate arrays now we know the number of columns
+  allocate(row(2*nc))
+  allocate(col(2*nc))
+  row(1:nc) = edgey(1,:nc); row(nc+1:2*nc) = edgey(2,:nc)
+  col(1:nc) = edgey(2,:nc); col(nc+1:2*nc) = edgey(1,:nc)
+
+  ! allocate(row, source=reshape(edgey(:,:nc), [ 2*nc ]))
+  ! allocate(col, source=reshape(edgey(:,:nc), [ 2*nc ]))
+
+  write(*,*) "This is the length of row and col", 2*nc, size(row), size(col)
+  nc = nc*2 ! nc is now the length of row and col vectors
+
+  write(*,*) "Shape of nc", nc
+
+  ! allocate these for insertion to cloud
+  allocate(sort(nc))
+  allocate(bnnz(nc))
+  allocate(bnnzsum(nc))
+  allocate(colsum(nc))
+
+  do i = 1, nc
+    sort(i) = i
+    colsum(i) = i
+  end do
+
+  k = nc
+
+  ! sort these arrays
+  call sort_quick(nc, row, sort, 1, k)
+  col = col(sort)
+
+  write(*,*) "sorted arrays", nc, size(nnz), maxval(row), maxval(col), maxval(edgey)
+
+  ! get the number of nonzeros per row
+  call bincount(nc, row, nnz)
+
+  write(*,*) "bincount nnz", size(nnz), minval(nnz), maxval(nnz)
+
+  ! get the cumulative sum of nnz
+  call cumsum(nc, nnz, nnzsum)
+
+  write(*,*) "cumsum nnz", size(nnzsum), minval(nnzsum), maxval(nnzsum)
+
+  k = maxval(nnz) ! max number of columns in cloud
+
+  write(*,*) "bnnz", size(bnnz), size(nnz), nnz(n-1), nnz(n)
+
+  ! place this in a vector where the number of nonzeros are
+  ! subtracted from the number of columns
+  bnnz = 0
+  bnnz(nnzsum(:nc-1)) = k - nnz(:n-1)
+  call cumsum(nc, bnnz, bnnzsum)
+
+  colsum = colsum + bnnzsum
+
+  write(*,*) "colsum", maxval(colsum)
+
+
+
+  ! allocate(nmask(k*n))
+  ! nmask(:) = .true.
+  ! allocate(cloud, source=reshape(pack(col, .true., colsum), (/n,k/)))
+  allocate(cloud(k*n))
+  cloud = 0
+  write(*,*) "allocated cloud", k, n
+  cloud(colsum) = col
+  write(*,*) "shoving it in is done", n, k, maxval(cloud)
+  nk = k*n
+  call callable(nk,cloud)
+  ! cloud = pack(col, .true., (/n,k/))
+  write(*,*) "cloud", cloud(0)
+
+  ! allocate(cloud(n,k))
+  ! cloud(colsum) = col
+  ! cloud = reshape(cloud, (/ n, k /))
+  ! allocate(nmask(k*n))
+  ! nmask(colsum) = .true.
+  ! nmask = reshape(nmask, (/ n, k /))
+
+  ! return
+end subroutine tricloud2
+! end module something
 
 subroutine tricloud ( nt, ltri, n, ncol, cloud, kmax )
 ! Finds all neighbours and extended neighbours for every point
