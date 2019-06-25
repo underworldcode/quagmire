5d7ec3a1fda74a55a1964ddc2417da9669522bbe
Ben Mather <brmather1@gmail.com>
2019-06-25 15:02:32 +1000

    Update setup.py for better dependency handling

--- quagmire-src/setup.py
+++ quagmire-src/setup.py
@@ -4,27 +4,38 @@ from os import path
 import io
 
 ext = Extension(name    = 'quagmire._fortran',
-                        sources = ['fortran/quagmire.pyf','fortran/trimesh.f90'])
+                sources = ['fortran/quagmire.pyf','fortran/trimesh.f90'])
 
 
 this_directory = path.abspath(path.dirname(__file__))
 with io.open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
-        long_description = f.read()
+    long_description = f.read()
 
 
 if __name__ == "__main__":
     setup(name = 'quagmire',
-          author            = "Louis Moresi",
-          author_email      = "louis.moresi@unimelb.edu.au",
+          author            = "Ben Mather",
+          author_email      = "ben.mather@sydney.edu.au",
           url               = "https://github.com/underworldcode/quagmire",
           version           = "0.7.0",
           description       = "Python surface process framework on highly scalable unstructured meshes",
           long_description  = long_description,
           long_description_content_type='text/markdown',
           ext_modules       = [ext],
-          packages          = ['quagmire', 'quagmire.tools', 'quagmire.equation_systems', 'quagmire.function', 'quagmire.scaling', 'quagmire.mesh', 'quagmire.topomesh', 'quagmire.surfmesh'],
+          packages          = ['quagmire',
+                               'quagmire.tools',
+                               'quagmire.equation_systems',
+                               'quagmire.function',
+                               'quagmire.scaling',
+                               'quagmire.mesh',
+                               'quagmire.topomesh',
+                               'quagmire.surfmesh'],
           install_requires  = ['numpy', 'stripy'],
+          python_requires   = '>=2.7, >=3.5',
+          setup_requires    = ['pytest-runner', 'webdav'],
+          tests_require     = ['pytest', 'webdav'],
           package_data      = {'quagmire': ['Examples/Notebooks/data',
+                                            'Examples/Notebooks/*.ipynb',
                                             'Examples/Notebooks/WorkedExamples/*.ipynb',  ## Leave out Unsupported
                                             'Examples/Notebooks/Tutorial/*.ipynb',
                                             'Examples/Scripts/IdealisedExamples',
@@ -32,12 +43,9 @@ if __name__ == "__main__":
                                             'Examples/Scripts/LandscapePreprocessing',    ## Leave out Unsupported
                                             'Examples/Scripts/Scripts/*.py']},
           classifiers       = ['Programming Language :: Python :: 2',
-                               'Programming Language :: Python :: 2.6',
                                'Programming Language :: Python :: 2.7',
                                'Programming Language :: Python :: 3',
-                               'Programming Language :: Python :: 3.3',
-                               'Programming Language :: Python :: 3.4',
                                'Programming Language :: Python :: 3.5',
                                'Programming Language :: Python :: 3.6',
-                               'Programming Language :: Python :: 3.7s',]
+                               'Programming Language :: Python :: 3.7',]
           )
